77816f49287e18af6522a2b0bd78227e108aad1d
sunchao <sun.shine23@163.com>
2017-11-27 11:32:49 +0800

    新增修改文件

---
+++ 华夏基金工作日志/恒生UF2.0国金证券/恒生UF2.0(国金证券)V1.0.mm
@@ -0,0 +1,139 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1509003604961" ID="ID_706829414" MODIFIED="1509412089488" TEXT="&#x6052;&#x751f;UF2.0(&#x56fd;&#x91d1;&#x8bc1;&#x5238;)">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1509089621312" ID="ID_106577214" MODIFIED="1509412089488" POSITION="right" TEXT="&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1509003706140" ID="ID_256835403" MODIFIED="1509412089488" TEXT="&#x9ed8;&#x8ba4;&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1509089131887" ID="ID_945145620" MODIFIED="1509412089488" TEXT="&#x666e;&#x901a;&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509003725020" ID="ID_987008583" MODIFIED="1509412089488" TEXT="&#x6e2f;&#x80a1;&#x901a;&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509003805571" ID="ID_1985693075" MODIFIED="1509412089488" TEXT="&#x5927;&#x5b97;&#x4ea4;&#x6613;&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509002838642" ID="ID_1196832353" MODIFIED="1509412089488" TEXT="&#x914d;&#x80a1;&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509003908043" ID="ID_462320879" MODIFIED="1509412089488" TEXT="ETF&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1509089219447" ID="ID_394442314" MODIFIED="1509412089488" TEXT="ETF&#x7533;&#x8bf7;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509089223624" ID="ID_1381475289" MODIFIED="1509412089487" TEXT="ETF&#x8d4e;&#x56de;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1509089165031" ID="ID_577472312" MODIFIED="1509412089487" TEXT="&#x6df1;&#x4ea4;&#x6240;&#x5206;&#x7ea7;&#x57fa;&#x91d1;&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1509003858355" ID="ID_1274639547" MODIFIED="1509412089487" TEXT="&#x6df1;&#x4ea4;&#x6240;&#x5b50;&#x57fa;&#x91d1;&#x5408;&#x5e76;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509003866323" ID="ID_1272661303" MODIFIED="1509412089487" TEXT="&#x6df1;&#x4ea4;&#x6240;&#x6bcd;&#x57fa;&#x91d1;&#x62c6;&#x5206;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1509089651014" ID="ID_1420204687" MODIFIED="1509412089487" TEXT="&#x878d;&#x8d44;&#x878d;&#x5238;&#x59d4;&#x6258;&#x786e;&#x8ba4;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509089735231" ID="ID_1663297012" MODIFIED="1509412089487" TEXT="&#x73b0;&#x91d1;&#x8fd8;&#x6b3e;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509089739191" ID="ID_169972353" MODIFIED="1509412089487" TEXT="&#x73b0;&#x5238;&#x8fd8;&#x5238;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1509089777008" ID="ID_878929186" MODIFIED="1509412089487" POSITION="right" TEXT="&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1509003752819" ID="ID_1901915390" MODIFIED="1509412089487" TEXT="&#x9ed8;&#x8ba4;&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1509003953995" ID="ID_1224872863" MODIFIED="1509412089486" TEXT="&#x666e;&#x901a;&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509003778588" ID="ID_1966959168" MODIFIED="1509412089486" TEXT="&#x6e2f;&#x80a1;&#x901a;&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509003820675" ID="ID_236537918" MODIFIED="1509412089486" TEXT="&#x5927;&#x5b97;&#x4ea4;&#x6613;&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509002846466" ID="ID_953539812" MODIFIED="1509412089486" TEXT="&#x914d;&#x503a;&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509089369718" ID="ID_1321821247" MODIFIED="1509412089486" TEXT="&#x6df1;&#x4ea4;&#x6240;&#x5206;&#x62c6;&#x5408;&#x5e76;&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1509089846936" ID="ID_1618804121" MODIFIED="1509412089486" TEXT="&#x878d;&#x8d44;&#x878d;&#x5238;&#x59d4;&#x6258;&#x64a4;&#x5355;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1509004345571" ID="ID_1846154927" MODIFIED="1509412089486" POSITION="left" TEXT="&#x8d44;&#x91d1;&#x8d26;&#x6237;&#x767b;&#x5f55;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1509004350731" ID="ID_1899303114" MODIFIED="1509412089486" TEXT="&#x8d26;&#x6237;&#x767b;&#x5f55;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1509004359501" ID="ID_601396703" MODIFIED="1509413039359" TEXT="&#x8d26;&#x6237;&#x767b;&#x51fa;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1509004168452" ID="ID_823085829" MODIFIED="1509412089486" POSITION="left" TEXT="&#x67e5;&#x8be2;&#x4ea4;&#x6613;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1509004188500" ID="ID_1102814833" MODIFIED="1509412089485" TEXT="&#x65b0;&#x80a1;&#x4fe1;&#x606f;&#x67e5;&#x8be2;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1509004220763" ID="ID_180456555" MODIFIED="1509412089485" TEXT="&#x67e5;&#x8be2;&#x5ba2;&#x6237;&#x5bf9;&#x8d26;&#x4fe1;&#x606f;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1509004238571" ID="ID_1157529592" MODIFIED="1509412089485" TEXT="&#x67e5;&#x8be2;&#x5ba2;&#x6237;&#x6240;&#x6709;&#x8bc1;&#x5238;&#x59d4;&#x6258;&#x4fe1;&#x606f;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1509004250900" ID="ID_952556561" MODIFIED="1509412089485" TEXT="&#x67e5;&#x8be2;&#x5ba2;&#x6237;&#x6700;&#x5927;&#x53ef;&#x7528;&#x989d;&#x5ea6;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1509004260811" ID="ID_1343967843" MODIFIED="1509412089485" TEXT="&#x67e5;&#x8be2;&#x5ba2;&#x6237;&#x6210;&#x4ea4;&#x56de;&#x62a5;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1509287001232" ID="ID_1869843538" MODIFIED="1509412089485" TEXT="&#x67e5;&#x8be2;&#x5206;&#x7ea7;&#x57fa;&#x91d1;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1509004330747" ID="ID_9370878" MODIFIED="1509412089483" TEXT="&#x67e5;&#x8be2;ETF&#x4fe1;&#x606f;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1509171502120" ID="ID_147493145" MODIFIED="1509412089483" TEXT="&#x67e5;&#x8be2;&#x6caa;&#x6e2f;&#x901a;&#x4fe1;&#x606f;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1509004287852" ID="ID_872651146" MODIFIED="1509412089483" TEXT="&#x878d;&#x8d44;&#x878d;&#x5238;&#x67e5;&#x8be2;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1508986747391" ID="ID_554780530" MODIFIED="1509412089483" TEXT="&#x878d;&#x8d44;&#x878d;&#x5238;&#x5408;&#x7ea6;&#x67e5;&#x8be2;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1508997336534" ID="ID_113913037" MODIFIED="1509412089483" TEXT="&#x67e5;&#x8be2;&#x53ef;&#x878d;&#x5238;&#x6570;&#x91cf;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1508986781142" ID="ID_1502696402" MODIFIED="1509412089482" TEXT="&#x878d;&#x8d44;&#x878d;&#x5238;&#x4fe1;&#x606f;&#x67e5;&#x8be2;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1509004284388" ID="ID_895974525" MODIFIED="1509412089471" TEXT="&#x914d;&#x80a1;&#x914d;&#x503a;&#x989d;&#x5ea6;&#x67e5;&#x8be2;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</map>
