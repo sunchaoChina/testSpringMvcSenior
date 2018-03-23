d7834647b04a2e9388599c17464f4a352d1daf1e
sunchao <sun.shine23@163.com>
2018-03-23 14:52:43 +0800

    添加项目

---
+++ test_exception/src/main/java/testException/test_exception/App.java
@@ -0,0 +1,13 @@
+package testException.test_exception;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
