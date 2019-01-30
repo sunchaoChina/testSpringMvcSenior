2f83d2b61697260efea08e8bc66dea91afdece6f
sunchao_hp <sunchao988@outlook.com>
2019-01-30 21:50:46 +0800

    设计模式提交

---
+++ 华夏基金工作日志/设计模式.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1546690817539" ID="ID_1996394016" MODIFIED="1546766837605" TEXT="&#x8bbe;&#x8ba1;&#x6a21;&#x5f0f;">
+<node CREATED="1546691109805" ID="ID_829175412" MODIFIED="1546691168893" POSITION="right" TEXT="&#x7b56;&#x7565;&#x6a21;&#x5f0f;">
+<node CREATED="1546691115205" ID="ID_1508496574" MODIFIED="1546691165342" TEXT="&#x5b9a;&#x4e49;&#x4e86;&#x7b97;&#x6cd5;&#x7c07;&#xff0c;&#x5206;&#x522b;&#x5c01;&#x88c5;&#x8d77;&#x6765;&#xff0c;&#x8ba9;&#x5b83;&#x4eec;&#x4e4b;&#x95f4;&#x53ef;&#x4ee5;&#x4e92;&#x76f8;&#x66ff;&#x6362;&#xff0c;&#x6b64;&#x6a21;&#x5f0f;&#x8ba9;&#x7b97;&#x6cd5;&#x7684;&#x53d8;&#x5316;&#x72ec;&#x7acb;&#x4e8e;&#x4f7f;&#x7528;&#x7b97;&#x6cd5;&#x7684;&#x5ba2;&#x6237;"/>
+</node>
+<node CREATED="1546690826823" ID="ID_1180641161" MODIFIED="1546690835131" POSITION="right" TEXT="&#x89c2;&#x5bdf;&#x8005;&#x6a21;&#x5f0f;">
+<node CREATED="1546690870894" ID="ID_1550129167" MODIFIED="1546690924362" TEXT="&#x5b9a;&#x4e49;&#x4e86;&#x5bf9;&#x8c61;&#x4e4b;&#x95f4;&#x4e00;&#x5bf9;&#x591a;&#x4f9d;&#x8d56;&#xff0c;&#x8fd9;&#x6837;&#x4e00;&#x6765;&#xff0c;&#x5f53;&#x4e00;&#x4e2a;&#x5bf9;&#x8c61;&#x6539;&#x53d8;&#x72b6;&#x6001;&#x65f6;&#xff0c;&#x5b83;&#x7684;&#x6240;&#x6709;&#x4f9d;&#x8d56;&#x7740;&#x90fd;&#x4f1a;&#x6536;&#x5230;&#x901a;&#x77e5;&#x5e76;&#x81ea;&#x52a8;&#x66f4;&#x65b0;"/>
+<node CREATED="1546691213589" ID="ID_1187274239" MODIFIED="1546691220451" TEXT="java&#x5185;&#x7f6e;&#x7684;&#x89c2;&#x5bdf;&#x8005;&#x6a21;&#x5f0f;">
+<node CREATED="1546738481899" ID="ID_1616704870" MODIFIED="1546739578774" TEXT="java.util.Observable&#x53ef;&#x89c2;&#x5bdf;&#x8005;&#x7c7b;&#xff0c;&#x8ffd;&#x8e2a;&#x6240;&#x6709;&#x89c2;&#x5bdf;&#x8005;&#x5e76;&#x901a;&#x77e5;&#x5b83;&#x4eec;"/>
+<node CREATED="1546738498005" ID="ID_424476434" MODIFIED="1546738715141" TEXT="java.util.Observer&#x89c2;&#x5bdf;&#x8005;&#x63a5;&#x53e3;"/>
+</node>
+<node CREATED="1546741069767" ID="ID_1104177735" MODIFIED="1546741086947" TEXT="JDK&#x4e2d;&#x7684;&#x89c2;&#x5bdf;&#x8005;&#x6a21;&#x5f0f;">
+<node CREATED="1546741198839" ID="ID_1691850650" MODIFIED="1546741215714" TEXT="JavaBeans"/>
+<node CREATED="1546741203798" ID="ID_320277198" MODIFIED="1546741205778" TEXT="swing"/>
+</node>
+</node>
+<node CREATED="1546766840707" ID="ID_1127770939" MODIFIED="1546766847711" POSITION="right" TEXT="&#x88c5;&#x9970;&#x7740;&#x6a21;&#x5f0f;">
+<node CREATED="1546768138831" ID="ID_1541851909" MODIFIED="1546768201304" TEXT="&#x52a8;&#x6001;&#x5730;&#x5c06;&#x8d23;&#x4efb;&#x9644;&#x52a0;&#x5230;&#x5bf9;&#x8c61;&#x4e0a;&#x3002;&#x82e5;&#x8981;&#x6269;&#x5c55;&#x529f;&#x80fd;&#xff0c;&#x88c5;&#x9970;&#x8005;&#x63d0;&#x4f9b;&#x4e86;&#x6bd4;&#x7ee7;&#x627f;&#x66f4;&#x6709;&#x5f39;&#x6027;&#x7684;&#x66ff;&#x4ee3;&#x65b9;&#x6848;"/>
+<node CREATED="1547645293860" ID="ID_1750581775" MODIFIED="1547645310229" TEXT="JDK&#x4e2d;&#x7684;&#x88c5;&#x9970;&#x8005;&#x6a21;&#x5f0f;">
+<node CREATED="1547645312825" ID="ID_1627044451" MODIFIED="1547645368404" TEXT="java&#x4e2d;&#x6709;&#x5f88;&#x591a;&#x88c5;&#x9970;&#x7740;&#x6a21;&#x5f0f;&#xff0c;&#x6bd4;&#x5982;Java I/O"/>
+</node>
+</node>
+<node CREATED="1546690938735" ID="ID_567177889" MODIFIED="1546690943287" POSITION="left" TEXT="&#x8bbe;&#x8ba1;&#x539f;&#x5219;">
+<node CREATED="1546690993686" ID="ID_986628786" MODIFIED="1546691040205" TEXT="&#x627e;&#x51fa;&#x5e94;&#x7528;&#x4e2d;&#x53ef;&#x80fd;&#x9700;&#x8981;&#x53d8;&#x5316;&#x4e4b;&#x5904;&#xff0c;&#x628a;&#x5b83;&#x4eec;&#x72ec;&#x7acb;&#x51fa;&#x6765;&#xff0c;&#x4e0d;&#x8981;&#x548c;&#x90a3;&#x4e9b;&#x4e0d;&#x9700;&#x8981;&#x53d8;&#x5316;&#x7684;&#x4ee3;&#x7801;&#x6df7;&#x5728;&#x4e00;&#x8d77;"/>
+<node CREATED="1546691049373" ID="ID_429114370" MODIFIED="1546691070919" TEXT="&#x9488;&#x5bf9;&#x63a5;&#x53e3;&#x7f16;&#x7a0b;&#xff0c;&#x800c;&#x4e0d;&#x662f;&#x9488;&#x5bf9;&#x5b9e;&#x73b0;&#x7f16;&#x7a0b;"/>
+<node CREATED="1546691081941" ID="ID_1169638072" MODIFIED="1546691090480" TEXT="&#x591a;&#x7528;&#x7ec4;&#x5408;&#xff0c;&#x5c11;&#x7528;&#x7ee7;&#x627f;"/>
+<node CREATED="1546690944214" ID="ID_1336521928" MODIFIED="1546690966659" TEXT="&#x4e3a;&#x4e86;&#x4ea4;&#x4e92;&#x5bf9;&#x8c61;&#x4e4b;&#x95f4;&#x7684;&#x677e;&#x8026;&#x5408;&#x8bbe;&#x8ba1;&#x800c;&#x52aa;&#x529b;"/>
+<node CREATED="1546766863946" ID="ID_223548532" MODIFIED="1546766878483" TEXT="&#x7c7b;&#x5e94;&#x8be5;&#x5bf9;&#x6269;&#x5c55;&#x5f00;&#x653e;&#xff0c;&#x5bf9;&#x4fee;&#x6539;&#x5173;&#x95ed;"/>
+</node>
+</node>
+</map>
